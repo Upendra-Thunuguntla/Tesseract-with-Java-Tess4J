24e3e83c27487b155a0ef230d3d57ca239308bcd
Muralidhar Thunuguntla <muralidhar.thunuguntla@gmail.com>
2021-07-06 07:58:59 +0530

    Initial Code commit

---
+++ Tess4j/src/main/java/upendra/tess4j/constants/Locations.java
@@ -0,0 +1,12 @@
+package upendra.tess4j.constants;
+
+import java.io.File;
+
+public class Locations {
+
+	public static final String PROCESSING_FOLDER = "Processing Folder",
+			INPUT 			= PROCESSING_FOLDER + File.pathSeparator + "Input",
+			OUTPUT			= PROCESSING_FOLDER + File.pathSeparator + "Output",
+			CONVERTED 		= PROCESSING_FOLDER + File.pathSeparator + "Converted",
+			TMP 			= PROCESSING_FOLDER + File.pathSeparator + "tmp";
+}
---
+++ Tess4j/src/main/java/upendra/tess4j/main/Main.java
@@ -0,0 +1,9 @@
+package upendra.tess4j.main;
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+    	
+    }
+}
