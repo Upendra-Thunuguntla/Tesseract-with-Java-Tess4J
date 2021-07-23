91226bccdcf7e1c663f4d1287dfbe7311f62df0c
Muralidhar Thunuguntla <muralidhar.thunuguntla@gmail.com>
2021-07-23 09:41:12 +0530

    Create Tools.java

---
+++ Tess4j/src/main/java/upendra/tess4j/utils/Tools.java
@@ -0,0 +1,37 @@
+package upendra.tess4j.utils;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class Tools {
+
+	public static String writeException(Exception e) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		e.printStackTrace(pw);
+		pw.flush();
+		pw.close();
+		return sw.toString();
+	}
+
+	public static String getFileName(File file) {
+		return getFileName(file.getName());
+	}
+
+	public static String getFileName(String fileName) {
+		int index = fileName.lastIndexOf(".");
+		if (index > 0) {
+			return fileName.substring(0,index);}
+		return fileName;
+	}
+
+	public static void wait(int duration) {
+		Long now = System.currentTimeMillis();
+		Long sleepTime = now + duration;
+
+		while(now<sleepTime) {
+			if(now != System.currentTimeMillis()) {
+				now = System.currentTimeMillis();}}
+	}
+}
