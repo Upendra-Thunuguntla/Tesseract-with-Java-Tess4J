54e35c8579b7d2e918bf7f4e5de2e26be86f4580
Muralidhar Thunuguntla <muralidhar.thunuguntla@gmail.com>
2021-07-23 16:40:14 +0530

    Update SplitAndProcess.java

--- Tess4j/src/main/java/upendra/tess4j/processors/SplitAndProcess.java
+++ Tess4j/src/main/java/upendra/tess4j/processors/SplitAndProcess.java
@@ -3,18 +3,63 @@ package upendra.tess4j.processors;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import com.recognition.software.jdeskew.ImageDeskew;
 
+import upendra.tess4j.constants.Constants;
+import upendra.tess4j.utils.Log;
+
 public class SplitAndProcess implements Runnable{
 
+	//Attributes
+	String filename;
+	String extention;
+	String outputFolderPath;
+	int i;
+	BufferedImage bi;
+
+
+
+	/**
+	 * @param filename
+	 * @param extention
+	 * @param outputFolderPath
+	 * @param i
+	 * @param bi
+	 */
+	public SplitAndProcess(String filename, String extention, String outputFolderPath, int i, BufferedImage bi) {
+		this.filename = filename;
+		this.extention = extention;
+		this.outputFolderPath = outputFolderPath;
+		this.i = i;
+		this.bi = bi;
+	}
 
 	//For Deskew
 	static final Double MIN_DESKEW_THRESHOLD = 0.05d;
 
+
+
+
 	@Override
 	public void run() {
 
+		File outputFile = new File(outputFolderPath+Constants.SEP+i+"."+extention);
+		try {
+			if (Constants.isDeskewEnabled.booleanValue())
+				ImageIO.write(deSkewImage(bi),extention,outputFile);
+			else
+				ImageIO.write(bi,extention,outputFile);
+			
+			outputFolderPath=null; bi=null;
+		} catch (IOException e) {
+			Log.error(filename + " : Error at page " + i + ". Cancelling Split Operation");
+			throw new RuntimeException(e.getMessage());
+		}
 	}
 
 	public static BufferedImage deSkewImage(BufferedImage bi) {
