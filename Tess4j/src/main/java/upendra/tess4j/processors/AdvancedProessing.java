5465fb9f9b2af640c1ca5a276d5171139490f7e0
Muralidhar Thunuguntla <muralidhar.thunuguntla@gmail.com>
2021-07-23 16:45:25 +0530

    Create AdvancedProessing.java

---
+++ Tess4j/src/main/java/upendra/tess4j/processors/AdvancedProessing.java
@@ -0,0 +1,91 @@
+package upendra.tess4j.processors;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+
+import upendra.tess4j.constants.Constants;
+import upendra.tess4j.utils.Log;
+import upendra.tess4j.utils.Tools;
+
+public class AdvancedProessing implements Runnable{
+
+	//For Conversion
+	File inputTifFile;
+	String outputPDFPath;
+	String filename_without_extention;
+
+	//For Splitting and Merging
+	String extention = "tif";
+	File splitTIFLocation;
+	File splitPDFLocation;
+
+
+
+	//For Monitoring
+	Boolean wait;
+
+	@Override
+	public void run() {
+
+	}
+
+
+	public void processTIFFile() {
+
+		try {
+			ImageInputStream is = ImageIO.createImageInputStream(inputTifFile);
+			if ( is == null || is.length() == 0) {
+				throw new NullPointerException("Empty File");
+			}
+			Iterator<ImageReader> iter = ImageIO.getImageReaders(is);
+			if (iter == null || !iter.hasNext()) {
+				throw new IOException("Image file format not supported by ImageIO : "+inputTifFile.getName());
+			}
+
+			ImageReader reader = iter.next();
+
+			reader.setInput(is);
+
+			Integer totalPages = reader.getNumImages(true);
+			logInfo("Total Pages - " + totalPages);
+			splitPDFLocation = new File(Constants.TMP+Constants.SEP+filename_without_extention+Constants.SEP+"SplitPDFs");
+			splitTIFLocation = new File(Constants.TMP+Constants.SEP+filename_without_extention+Constants.SEP+"SplitTIFs");
+
+			splitPDFLocation.mkdirs();
+			splitTIFLocation.mkdirs();
+
+			ExecutorService ex = Executors.newFixedThreadPool(Constants.THREAD_COUNT_1);
+
+			for (int i=0;i<totalPages;i++) {
+				ex.execute(new SplitAndProcess(filename_without_extention, extention, splitTIFLocation.getAbsolutePath(), i, reader.read(i)));
+			}
+
+			ex.shutdown();
+			while(!ex.isTerminated()) {}
+
+		}catch (Exception e ) {
+			logError(e);
+			throw new RuntimeException(e.getMessage());
+		}
+	}
+
+
+	private void logInfo(String message) {
+		Log.info(inputTifFile.getName()+" : "+message);
+	}
+
+	private void logError(String message) {
+		Log.error(inputTifFile.getName()+" : "+message);
+	}
+
+	private void logError(Exception ex) {
+		logError(Tools.writeException(ex));
+	}
+}
